1acb1074e06fec224104ea543bf048bc41e18553
Marcley <marcleyrosa@gmail.com>
2022-09-21 13:21:29 -0300

    Requisito 03

--- src/pages/Table.jsx
+++ src/pages/Table.jsx
@@ -1,23 +1,117 @@
 import React, { useContext, useState } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
-function Login() {
+function StarWarsTable() {
   const { results } = useContext(StarWarsContext);
   const [filterName, setFilterName] = useState('');
-  const namesFilter = results
+  const [filterColumn, setFilterComumn] = useState('population');
+  const [comparisonFilter, setComparisonFilter] = useState('maior que');
+  const [filterValue, setFilterValue] = useState(0);
+  // const [renderFilters, setRenderFilters] = useState(false);
+
+  const [buttonFilter, setButtonFilter] = useState({
+    column: 'population',
+    comparison: 'maior que',
+    value: 0,
+  });
+
+  const filterNumberSwitch = () => {
+    const switchColumn = (result) => {
+      switch (buttonFilter.column) {
+      case 'population': return result.population;
+      case 'orbital_period': return result.orbital_period;
+      case 'diameter': return result.diameter;
+      case 'rotation_period': return result.rotation_period;
+      default: return result.surface_water;
+      }
+    };
+
+    const maiorQue = results
+      .filter((result) => +switchColumn(result) > +buttonFilter.value)
+      .filter((unk) => switchColumn(unk) !== 'unknown');
+    const menorQue = results
+      .filter((result) => +switchColumn(result) < +buttonFilter.value);
+    const igualA = results
+      .filter((result) => +switchColumn(result) === +buttonFilter.value);
+
+    if (buttonFilter.isClick) {
+      // setRenderFilters((prevState) => ({
+      //   ...prevState,
+      //   list: [buttonFilter],
+      // }));
+      console.log('entrei');
+      switch (buttonFilter.comparison) {
+      case 'menor que': return menorQue;
+      case 'igual a': return igualA;
+      default: return maiorQue;
+      }
+    } else {
+      return results;
+    }
+  };
+
+  console.log(filterNumberSwitch());
+
+  const namesFilter = filterNumberSwitch()
     .filter((e) => e.name.toUpperCase().includes(filterName.toUpperCase()));
+
   return (
     <div>
-      <label htmlFor="input-filter">
-        Filter
-        <input
-          data-testid="name-filter"
-          onChange={ ({ target }) => setFilterName(target.value) }
-          type="text"
-          id="input-filter"
-        />
-      </label>
-      <p>{filterName}</p>
+      <form action="">
+        <label htmlFor="input-filter">
+          Filter
+          <input
+            data-testid="name-filter"
+            onChange={ ({ target: { value } }) => setFilterName(value) }
+            type="text"
+            id="input-filter"
+            value={ filterName }
+          />
+        </label>
+        <select
+          onChange={ ({ target: { value } }) => setFilterComumn(value) }
+          data-testid="column-filter"
+          value={ filterColumn }
+        >
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+        <select
+          onChange={ ({ target: { value } }) => setComparisonFilter(value) }
+          data-testid="comparison-filter"
+          name=""
+          id=""
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <label htmlFor="inp">
+          <input
+            onChange={ ({ target: { value } }) => setFilterValue(value) }
+            value={ filterValue }
+            data-testid="value-filter"
+            type="number"
+            id="inp"
+          />
+        </label>
+        <button
+          onClick={ () => setButtonFilter({
+            column: filterColumn,
+            comparison: comparisonFilter,
+            value: filterValue,
+            isClick: true,
+          }) }
+          data-testid="button-filter"
+          type="button"
+        >
+          Filtrar
+        </button>
+      </form>
+      <p>{`Filtr${filterName}`}</p>
       <table>
         <thead>
           <tr>
@@ -60,4 +154,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default StarWarsTable;
